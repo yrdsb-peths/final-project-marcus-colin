a1cd568dc5a308292cdd5de2e55819c670852a28
MarcusChuck <348580788@gapps.yrdsb.ca>
2025-05-26 09:24:51 -0400

    changed enemy

--- Enemy.java
+++ Enemy.java
@@ -14,7 +14,7 @@ public class Enemy extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        setLocation(getX(), getY()+3);
     }
     
     public void moveEnemy()
