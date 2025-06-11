21292d257f35424603672eede170e3922a7494ae
MarcusChuck <348580788@gapps.yrdsb.ca>
2025-06-11 15:44:48 -0400

    Update TutorialScreen.java

--- TutorialScreen.java
+++ TutorialScreen.java
@@ -3,9 +3,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class tutorialScreen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Collin and Marcus) 
+ * @version (June 2025)
  */
+//adds tutorial screen texxt 
 public class TutorialScreen extends World
 {
 
