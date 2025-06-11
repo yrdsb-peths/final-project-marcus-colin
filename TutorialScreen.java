d29fac76c3d60fd5a2700c5cb12c99f8727f3629
vyin <limcolin0809@gmail.com>
2025-06-11 06:10:28 -0400

    updated tutorial

--- TutorialScreen.java
+++ TutorialScreen.java
@@ -12,23 +12,26 @@ public class TutorialScreen extends World
     private Label tutorialText1;
     private Label tutorialText2;
     private Label tutorialText3;
+    private Label tutorialText4;
 
     public TutorialScreen()
     {
         super(600, 400, 1);
 
         // Add tutorial instructions
-        tutorialText1 = new Label("Score 100 points to win!", 40);
-        tutorialText2 = new Label("Use ← and → to move left and right", 40);
-        tutorialText3 = new Label("Press space to shoot", 40);
+        tutorialText1 = new Label("Score 100 points to win!", 35);
+        tutorialText2 = new Label("Use ← and → to move left and right", 35);
+        tutorialText3 = new Label("Press space to shoot", 35);
+        tutorialText4 = new Label("Jett is 1 shot, Iso 2, and Cypher 5", 35);
 
         addObject(tutorialText1, getWidth()/2, 150);
         addObject(tutorialText2, getWidth()/2, 200);
         addObject(tutorialText3, getWidth()/2, 250);
+        addObject(tutorialText4, getWidth()/2, 300);
 
         // Add instruction to start the game
         Label continueLabel = new Label("Press <space> to start the game", 30);
-        addObject(continueLabel, getWidth()/2, 300);
+        addObject(continueLabel, getWidth()/2, 350);
         
         Actor chamber = new Actor() {}; // Empty actor to hold the image
         GreenfootImage myImage = new GreenfootImage("chambersmirk.jpg");
@@ -36,6 +39,7 @@ public class TutorialScreen extends World
         int myNewWidth = myImage.getWidth() / 4;
         myImage.scale(myNewWidth, myNewHeight);
         chamber.setImage(myImage);
+        
     }
 
     public void act()
